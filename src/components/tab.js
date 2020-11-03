155a82f6151667bdd2e9195e475d56b0c66aeeff
Jesse Parsons <savvyjesse@aol.com>
2020-11-02 20:52:10 -0500

    notes

--- src/components/tab.js
+++ src/components/tab.js
@@ -2,11 +2,13 @@ import { contact } from './tabs/contact';
 import { home } from './tabs/home';
 import { menu } from './tabs/menu';
 function clearTab() {
-  
+
 }
 function tab(content=home) {
   // content is html content, default will be home tab with no args   
   //clear values on page and append content
+  // on page load render home tab with home stuff, add listeners to tab buttons
+  // onclick, render contents of clicked tab through this funciton... clear first, run function of desired tab
   content();
 }
 
