23f7802c5930d9fab58ba87e2e76b8a297638a3a
Jesse Parsons <savvyjesse@aol.com>
2020-11-07 16:46:42 -0500

    redom

--- src/components/tabs/home.js
+++ src/components/tabs/home.js
@@ -1,9 +1,16 @@
 import { el, mount } from 'redom';
-
 function home() {
-  // home stuff
-  console.log('rendering home elements');
-
+  const tab = document.getElementById('tab');
+  const container = el(
+    'div', {
+      class: 'container text-center'
+    }
+  );
+  const jumbotron = el(
+    '.jumbotron.notice'
+  );
+  mount(container, jumbotron);
+  mount(tab, container);
 }
 
 export { home }
