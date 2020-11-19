e68ecce41611bc53a1de8b52f5b4799d187fdb8e
Jesse Parsons <savvyjesse@aol.com>
2020-11-18 19:50:53 -0500

    add pictures, add button to remove notice

--- src/components/tabs/contact.js
+++ src/components/tabs/contact.js
@@ -3,6 +3,32 @@ function contact() {
   // show location on google map, and add links for yelp maybe?
   // email contact form
   console.log('rendering contact elements');
+
+  // change carousel items to show 'local' chefs
+  let carousel = document.getElementById('carouselInner');
+  carousel.innerHTML = `
+    <div class="carousel-item active">
+      <img
+        class="d-block w-100"
+        src="../assets/images/chef-1.jpg"
+        alt="chef"
+      />
+    </div>
+    <div class="carousel-item">
+      <img
+        class="d-block w-100"
+        src="../assets/images/chef-3.jpg"
+        alt="chef"
+      />
+    </div>
+    <div class="carousel-item">
+      <img
+        class="d-block w-100"
+        src="../assets/images/chef-4.jpg"
+        alt="chef"
+      />
+    </div>
+  `
 }
 
 export { contact }
--- src/components/tabs/home.js
+++ src/components/tabs/home.js
@@ -1,30 +1,38 @@
 function home() {
   const tab = document.getElementById('tab');
   tab.innerHTML = `
-  <div class="container text-center">
-  <div class="jumbotron notice">
-    <h1 class="display-4">Notice</h1>
-    <p class="lead">
-      We are pleased to announce that we will resume normal operations on
-      July 28.<br />
-      We look forward to serving you once again
-    </p>
-    <hr class="my-4" />
-    <p>
-      We are committed to following state guidelines to provide safe
-      service for you.
-    </p>
-    <p class="lead">
-      <a
-        class="btn btn-primary btn-lg"
-        href="https://www.nh.gov/covid19/resources-guidance/businesses.htm"
-        role="button"
-        >Learn more</a
-      >
-    </p>
+  <div class="container text-center notice">
+    <div class="jumbotron notice">
+      <div class="row">
+        <i class="fa fa-times-circle fa-2x ml-auto" aria-hidden="true"></i>
+      </div>
+      <h1 class="display-4">Notice</h1>
+      <p class="lead">
+        We are pleased to announce that we will resume normal operations on
+        July 28.<br />
+        We look forward to serving you once again
+      </p>
+      <hr class="my-4" />
+      <p>
+        We are committed to following state guidelines to provide safe
+        service for you.
+      </p>
+      <p class="lead">
+        <a
+          class="btn btn-primary btn-lg"
+          href="https://www.nh.gov/covid19/resources-guidance/businesses.htm"
+          role="button"
+          >Learn more</a
+        >
+      </p>
+    </div>
   </div>
-</div>
 `;
+
+let closeNotice = document.getElementsByClassName('notice')[0];
+closeNotice.addEventListener('click', function() {
+  closeNotice.style = 'display: none';
+});
 }
 
 export { home }
