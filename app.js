31fbe6f1b2b5a085b905b2dda9a060d54da94d16
Jesse Parsons <savvyjesse@aol.com>
2020-11-21 15:15:56 -0500

    configure routes

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 
-//create express app
+// create express app
 const express = require('express');
 const app = express();
 const path = express.path;
+
 // express middleware
-// app.use(express.static(path.join(__dirname, 'dist')));
-// app.use(express.static('assets'));
 app.use(express.static('dist'));
 app.use(express.static('assets'));
 
-//define route
+// define route
 app.get('/', (req, res) => {
   res.sendFile('./dist/index.html');
 });
 
-//server
+app.get('*', function(req, res){
+  res.send('Invalid URL');
+});
+
+// server
 app.listen(process.env.PORT || 3000, 
   () => console.log('server running'));
